26de8f374888849c1c3a520b3538b2545296580e
Radames Ajna <radamajna@gmail.com>
2018-06-01 14:00:03 -0700

    change title on navbar

--- src/components/MHeader.vue
+++ src/components/MHeader.vue
@@ -46,8 +46,8 @@
       </div>
     </div>
     <div id="menu" class="no-margin">
-      <div class="d-flex justify-content-between bd-highlight mb-3">
-        <div class="p-2">
+      <div class="row my-3">
+        <div class="col-8 col-md-2">
           <a class="navbar-brand" href="https://www.youthradio.org" target="_blank">
             <svg viewBox="0 0 515.01251 245.6125" height="50">
               <g transform="matrix(1.25,0,0,-1.25,0,245.6125)" id="g10">
@@ -58,8 +58,13 @@
             </svg>
           </a>
         </div>
-        <div class="p-2 bd-highlight">
-          <nav class="navbar navbar-expand-sm navbar-custom">
+        <div class="col-12 order-2 col-md-6 order-md-1 text-md-center">
+          <h1>
+            <router-link :to="{ name: 'Home', params: {}}"><a class="nav-link">In Their Own Words</a></router-link>
+          </h1>
+        </div>
+        <div class="col-4 order-1 col-md-4 order-md-2">
+          <nav class="navbar navbar-expand-md navbar-custom">
             <div class="ml-auto">
               <button id="menu-btn" class="navbar-toggler custom-toggler"
                   type="button"
@@ -72,17 +77,27 @@
             <div class="collapse navbar-collapse text-right">
 
               <ul class="navbar-nav mt-2 mt-lg-0">
-                <li class="nav-item active">
-                  <router-link :to="{ name: 'Home', params: {}}"><a class="nav-link">Home</a></router-link>
-                </li>
                 <li class="nav-item">
                   <a class="nav-link" href="#"> More Coverage <span class="sr-only">(current)</span></a>
                 </li>
                 <li class="nav-item">
                   <router-link :to="{ name: 'Credits' }"> <a v-on:click="menuItemClicked" class="nav-link" >Credits</a></router-link>
                 </li>
-                <li class="nav-item">
-                  <a class="nav-link disabled" href="#">Share</a>
+                <li class="nav-item ml-auto">
+                  <div class="nav-link h-100 d-flex flex-wrap align-items-end">
+                    <div class="p-1">
+                      <a class="hand-over" title="Share on Twitter">
+                        <svg class="float-right" fill="#3ec0b3" width="20" height="20" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="White" x="0px" y="0px" viewBox="0 0 400 400" style="enable-background:new 0 0 400 400;" xml:space="preserve">
+                        <path class="st0" d="M400,400H0V0h400V400z M153.6,301.6c94.3,0,145.9-78.2,145.9-145.9c0-2.2,0-4.4-0.1-6.6  c10-7.2,18.7-16.3,25.6-26.6c-9.2,4.1-19.1,6.8-29.5,8.1c10.6-6.3,18.7-16.4,22.6-28.4c-9.9,5.9-20.9,10.1-32.6,12.4  c-9.4-10-22.7-16.2-37.4-16.2c-28.3,0-51.3,23-51.3,51.3c0,4,0.5,7.9,1.3,11.7c-42.6-2.1-80.4-22.6-105.7-53.6  c-4.4,7.6-6.9,16.4-6.9,25.8c0,17.8,9.1,33.5,22.8,42.7c-8.4-0.3-16.3-2.6-23.2-6.4c0,0.2,0,0.4,0,0.7c0,24.8,17.7,45.6,41.1,50.3  c-4.3,1.2-8.8,1.8-13.5,1.8c-3.3,0-6.5-0.3-9.6-0.9c6.5,20.4,25.5,35.2,47.9,35.6c-17.6,13.8-39.7,22-63.7,22  c-4.1,0-8.2-0.2-12.2-0.7C97.7,293.1,124.7,301.6,153.6,301.6"/>
+                        </svg>
+                      </a>
+                    </div>
+                    <div class="p-1 ">
+                      <a class="hand-over" title="Share on Facebook">
+                        <svg class="float-right mr-1" fill="#3ec0b3" width="20" height="20" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 58 58"><path class="cls-1" d="M53.85,0H3.15A3.15,3.15,0,0,0,0,3.15v50.7A3.15,3.15,0,0,0,3.15,57h27.3V35H23V26.33h7.41V20c0-7.37,4.49-11.38,11.06-11.38A62.15,62.15,0,0,1,48.15,9v7.69H43.61c-3.57,0-4.26,1.69-4.26,4.18v5.5H47.9L46.79,35H39.35V57h14.5A3.15,3.15,0,0,0,57,53.85V3.15A3.15,3.15,0,0,0,53.85,0Z"/></svg>
+                      </a>
+                    </div>
+                  </div>
                 </li>
               </ul>
             </div>
@@ -186,7 +201,9 @@ p {
 .navbar-custom .nav-item:hover .nav-link {
   color: #000;
 }
-
+.navbar-collapse {
+  flex-grow: unset;
+}
 /* } */
 /* @media (min-width: 576px) {
   h3, h3 a {
